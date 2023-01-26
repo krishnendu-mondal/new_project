fa5318ab2715a807f7f92d49f5fa02849cf1d648
Krishnendu Mondal <70332046+krishnendu-mondal@users.noreply.github.com>
2023-01-26 22:31:03 +0530

    Add files via upload

--- messideba.c
+++ messideba.c
@@ -1,8 +1,9 @@
-#include<stdio.h>
+#include <stdio.h>
 
-//checking armstrong or not
-void checkArmstrong(int num){
-    int cpynum,mul=1,len=0,rem,sum=0;
+// checking armstrong or not
+void checkArmstrong(int num)
+{
+    int cpynum, mul = 1, len = 0, rem, sum = 0;
     cpynum = num;
     while (num != 0)
     {
@@ -23,21 +24,64 @@ void checkArmstrong(int num){
         sum += mul;
         mul = 1;
     }
-    if(sum==cpynum)
-        printf("%d ",sum);
+    if (sum == cpynum)
+        printf("%d ", sum);
 }
 
-//print armstrong in range
-void findArmstrong(int a, int b){
+// print armstrong in range
+void findArmstrongInRange(int a, int b)
+{
 
-    for(int i=a;i<=b;i++){
+    for (int i = a; i <= b; i++)
+    {
         checkArmstrong(i);
     }
 }
 
-//main function
-void main(){
-    int lowerlim, upperlim;
-    scanf("%d %d",&lowerlim,&upperlim);
-    findArmstrong(lowerlim,upperlim);
+void nthArmstrong(int f)
+{   int counter=0;
+    for (int i = 0; i <= 1000000000; i++)
+    {
+        int cpynum, num = i, mul = 1, len = 0, rem, sum = 0;
+        cpynum = num;
+        while (num != 0)
+        {
+            num = num / 10;
+            len++;
+        }
+
+        num = cpynum;
+
+        while (num != 0)
+        {
+            rem = num % 10;
+            num = num / 10;
+            for (int i = 1; i <= len; i++)
+            {
+                mul *= rem;
+            }
+            sum += mul;
+            mul = 1;
+        }
+        if (sum == cpynum)
+        {
+            if(counter!=f-1){
+                counter++;
+            }
+            else{
+                printf("%d th Armstrong number is : %d", counter+1, sum);
+                break;
+            } 
+        }
+    }  
+}
+
+// main function
+void main()
+{
+    //  int lowerlim, upperlim;    // int num;
+    //  scanf("%d %d",&lowerlim,&upperlim);    //  scanf("%d", &num);
+    //  findArmstrongInRange(lowerlim,upperlim);
+    //  nthArmstrong(num);
+    //  checkArmstrong(num);
 }
